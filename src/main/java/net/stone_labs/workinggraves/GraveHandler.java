0852e934a0549703217b8e13ebff35d8c3e848dd
StoneLabs <levyehrstein@googlemail.com>
2021-11-14 13:40:06 +0100

    Add 'gravesRequireSoulTorch' gamerule

--- src/main/java/net/stone_labs/workinggraves/GraveHandler.java
+++ src/main/java/net/stone_labs/workinggraves/GraveHandler.java
@@ -1,13 +1,15 @@
 package net.stone_labs.workinggraves;
 
-import net.minecraft.block.AbstractSignBlock;
 import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.item.Items;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+
 public class GraveHandler
 {
+    public static boolean requireSoulTorch = true;
+
     public static GraveManager getManager(ServerWorld world)
     {
         return world.getPersistentStateManager().getOrCreate((nbtCompound) -> {
@@ -19,7 +21,7 @@ public class GraveHandler
 
     public static void Interact(ServerPlayerEntity player, ServerWorld world, SignBlockEntity sign)
     {
-        if (!player.getMainHandStack().getItem().equals(Items.SOUL_TORCH))
+        if (requireSoulTorch && !player.getMainHandStack().getItem().equals(Items.SOUL_TORCH))
             return;
 
         if (!Grave.isGraveSign(sign))
--- src/main/java/net/stone_labs/workinggraves/WorkingGraves.java
+++ src/main/java/net/stone_labs/workinggraves/WorkingGraves.java
@@ -2,7 +2,10 @@ package net.stone_labs.workinggraves;
 
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory;
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -11,6 +14,7 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import net.stone_labs.workinggraves.commands.GravesCommand;
 import org.apache.logging.log4j.Level;
@@ -45,7 +49,26 @@ public class WorkingGraves implements DedicatedServerModInitializer
     {
         LOGGER.log(Level.INFO, "Initialized {} version {}", MOD_NAME, VERSION);
 
+        // Register command
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> GravesCommand.register(dispatcher));
+
+        // Register Right click handler
         UseBlockCallback.EVENT.register(new PlayerUseBlockEvent());
+
+        // Set values from gamerules on server start
+        ServerLifecycleEvents.SERVER_STARTED.register(server ->
+        {
+            GraveHandler.requireSoulTorch = server.getGameRules().get(REQUIRE_SOUL_TORCH).get();
+        });
+    }
+
+    public static final GameRules.Key<GameRules.BooleanRule> REQUIRE_SOUL_TORCH = register("gravesRequireSoulTorch", GameRules.Category.PLAYER, GameRuleFactory.createBooleanRule(true, (server, rule) ->
+    {
+        GraveHandler.requireSoulTorch = rule.get();
+    }));
+
+    private static <T extends GameRules.Rule<T>> GameRules.Key<T> register(String name, GameRules.Category category, GameRules.Type<T> type)
+    {
+        return GameRuleRegistry.register(name, category, type);
     }
 }
